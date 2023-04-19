756b9e357a7efa318d07ad5c90b31ba6521dc0e9
Andi <andi8647@gmail.com>
2023-04-19 21:57:01 +0200

    removed models

---
+++ detect_football.sh
@@ -0,0 +1,1 @@
+python detect.py --source ../../data/t7/t7.mp4 --weights runs/train/exp12/weights/best.pt --save-txt --img 3264, 576
---
+++ simple_train.sh
@@ -0,0 +1,1 @@
+python train.py --img 3264 --batch 4 --epochs 50 --data ./ball_detections_data_2/data.yaml --weights "yolov5n.pt" --cache
